d5aabd8fd124aa05462c49eea18e330530ff11a7
Vishal Lakkapathri <vishal.lakkapathri@copart.com>
2019-06-09 16:59:15 +0530

    Added Home screen

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
+import HomeScreen from './app/HomeScreen';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => HomeScreen);
